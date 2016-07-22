056afeb3f040508cb51e7b56ff34f238b2178872
tehnuk <tehik2015@yandex.ru>
2016-07-23 01:35:57 +0300

    firdt

---
+++ dance/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import include, url
+#from django.contrib import admin
+import django.contrib.auth.views
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'dance.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
+    url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'', include('blog.urls')),
+]
